df30be05aa3166cf39bfc53e1b4cceea569016df
Missael Gomez <gomezm5@unlv.nevada.edu>
2019-10-16 10:47:13 -0700

    Added the default Drawer Layout.

--- Search/app/src/main/java/com/example/search/FullscreenActivity.java
+++ Search/app/src/main/java/com/example/search/FullscreenActivity.java
@@ -92,6 +92,7 @@ public class FullscreenActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_fullscreen);
 
+
         mVisible = true;
         mControlsView = findViewById(R.id.fullscreen_content_controls);
         mContentView = findViewById(R.id.fullscreen_content);
