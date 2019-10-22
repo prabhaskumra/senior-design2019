e2f4f374e6248c72412a1fe817b3d3fbaba67e7b
prabhaskumra <prabhas1998@gmail.com>
2019-10-22 11:06:05 -0700

    added screen

---
+++ SearchScreen/app/src/main/java/com/example/searchscreen/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.searchscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    Camera camera;
+
+    FrameLayout frameLayout;
+    ShowCamera showCamera;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        frameLayout = (FrameLayout)findViewById(R.id.frameLayout2);
+
+
+        // open the camera
+
+        camera = Camera.open();
+
+        showCamera = new ShowCamera(this, camera);
+        frameLayout.addView(showCamera);
+
+    }
+}
