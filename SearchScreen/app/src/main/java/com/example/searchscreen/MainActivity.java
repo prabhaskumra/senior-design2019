db8b0460c2cd5c43681e78d761bb9dfe76a8f8e2
Bryan Lubay <lubayb1@unlv.nevada.edu>
2019-11-08 17:44:29 -0800

    added functionality to open settings from main activity. next step is to be able to go back to previous activities. <3 :D

--- SearchScreen/app/src/main/java/com/example/searchscreen/MainActivity.java
+++ SearchScreen/app/src/main/java/com/example/searchscreen/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.searchscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,13 +20,17 @@ public class MainActivity extends AppCompatActivity {
     FrameLayout frameLayout;
     ShowCamera showCamera;
 
+    // vars for settings menu
+    public static RecyclerView settingsView, translateSettings, findSettings, text_to_speech_Settings;
+    public boolean settingsOpen = false;
+    public ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         frameLayout = (FrameLayout)findViewById(R.id.frameLayout2);
-
 
         // open the camera
 
@@ -28,5 +39,48 @@ public class MainActivity extends AppCompatActivity {
         showCamera = new ShowCamera(this, camera);
         frameLayout.addView(showCamera);
 
+        // stuff for settings menu
+        initImageBitmaps();
+        settingsView.setVisibility(View.INVISIBLE);
+        ImageButton settingsButton = findViewById(R.id.app_settings);
+        settingsButton.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            if (!settingsOpen){
+                settingsOpen = true;
+                frameLayout.setVisibility(View.INVISIBLE);
+                settingsView.setVisibility(View.VISIBLE);
+            }
+            else{
+                settingsOpen = false;
+                frameLayout.setVisibility(View.VISIBLE);
+                settingsView.setVisibility(View.INVISIBLE);
+            }
+        }
+    });
+
     }
+    private void initImageBitmaps(){
+
+
+        mImageUrls.add("https://en.meming.world/images/en/6/6e/Surprised_Pikachu.jpg");
+        mNames.add("Find");
+
+        mImageUrls.add("https://en.meming.world/images/en/6/6e/Surprised_Pikachu.jpg");
+        mNames.add("Text-to-Speech");
+
+        mImageUrls.add("https://en.meming.world/images/en/6/6e/Surprised_Pikachu.jpg");
+        mNames.add("Translate");
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        settingsView = findViewById(R.id.settings_menu);
+        SettingsViewAdapter adapter = new SettingsViewAdapter(mNames, mImageUrls, this);
+        settingsView.setAdapter(adapter);
+        settingsView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+
 }
