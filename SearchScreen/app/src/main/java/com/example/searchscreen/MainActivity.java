75396137bb50c125f2cf7198fc59eceac0313150
prabhaskumra <prabhas1998@gmail.com>
2019-11-21 12:51:26 -0800

    1. added functionality to the buttons. Now user can pick photos from either the camera or photo gallery. 2. Also now it asks for the permissons as pop up window instead of going manually and turning it on.

--- SearchScreen/app/src/main/java/com/example/searchscreen/MainActivity.java
+++ SearchScreen/app/src/main/java/com/example/searchscreen/MainActivity.java
@@ -1,28 +1,43 @@
 package com.example.searchscreen;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.pm.PackageManager;
 import android.app.SearchManager;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Camera camera;
+    private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_CODE = 1001;
+    private static final int GALLERY_REQUEST_CODE = 1002;
 
+    Camera camera;
     FrameLayout frameLayout;
     ImageView mImageView;
     ShowCamera showCamera;
+    Button mCaptureBtn;
+    Button mFromPhotos;
+    Uri image_uri;
 
     private EditText editTextInput;
     @Override
@@ -30,32 +45,71 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        frameLayout = findViewById(R.id.frameLayout2);
         mImageView = findViewById(R.id.image_view);
+        mCaptureBtn = findViewById(R.id.capture_image_btn);
+        mFromPhotos = findViewById(R.id.from_photos);
+//        frameLayout = findViewById(R.id.frameLayout2);
         editTextInput =  findViewById(R.id.editTextInput);
-
 
         final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
 
-
-//        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
-//                != PackageManager.PERMISSION_GRANTED){
-//
-//            if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.CAMERA)) {
-//
-//            } else {
-//                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
-//            }
-//        } else {
-//            // permissions already granted.
-//            // open the camera
-//            camera = Camera.open();
-//        }
-
-//         open the camera
         camera = Camera.open();
 
 
+//        function button to capture photos from camera. Also asks for permissions
+        mCaptureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if(checkSelfPermission(Manifest.permission.CAMERA) ==
+                            PackageManager.PERMISSION_DENIED) {
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+                        requestPermissions(permission, PERMISSION_CODE);
+                    }
+
+                    else {
+//                         permission already granted
+                        openCamera();
+                    }
+
+                }
+                else {
+                    openCamera();
+
+                }
+
+            }
+        });
+
+
+//        function button to pick photos from camera gallery. Also asks for permissions
+        mFromPhotos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                            PackageManager.PERMISSION_DENIED) {
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+                        requestPermissions(permission, PERMISSION_CODE);
+                    }
+
+                    else {
+//                         permission already granted
+                        pickFromGallery();
+                    }
+
+                }
+                else {
+                    pickFromGallery();
+
+                }
+
+            }
+        });
 
         showCamera = new ShowCamera(this, camera);
 //        frameLayout.addView(showCamera);
@@ -83,6 +137,82 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        // camera intent
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+    }
+
+    //    open photos from gallery
+    private void pickFromGallery(){
+
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+
+        String[] mimeTypes = {"image/jpeg", "image/png"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+        startActivityForResult(intent,GALLERY_REQUEST_CODE);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMISSION_CODE: {
+                if(grantResults.length > 0 && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED  ) {
+//                    openCamera();
+                }
+                else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case IMAGE_CAPTURE_CODE:
+                if (resultCode == RESULT_OK) {
+                    mImageView.setImageURI(image_uri);
+                }
+
+                break;
+            case GALLERY_REQUEST_CODE:
+//                if(resultCode == RESULT_OK){
+//                    mImageView.setImageURI(image_uri);
+//                }
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+                Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex((filePathColumn[0]));
+                String imgDecodableString = cursor.getString(columnIndex);
+
+                cursor.close();
+                mImageView.setImageBitmap(BitmapFactory.decodeFile(imgDecodableString));
+
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + requestCode);
+        }
+    }
+
 
 
 }
